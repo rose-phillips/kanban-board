ce4ea2a23171bd9dfc7f3e3ad220abfc312d39ac
Rose Phillips <rophillips@ybs.co.uk>
2026-06-25 16:32:18 +0100

    added handleSubmit function for new item form

--- src/components/AddIssueForm.tsx
+++ src/components/AddIssueForm.tsx
@@ -1,12 +1,29 @@
+import { useDispatch } from "react-redux";
+import { addItem } from "../redux/ItemSlice";
 import { Status } from "../types";
+import type {StatusType} from "../types"
 
 export const AddIssueForm = () => {
+
+const dispatch = useDispatch();
+
+const HandleSubmit = (data: FormData) => {
+    const name = data.get("name") as string;
+    const status = data.get("status") as StatusType;
+    dispatch(addItem({
+      id: crypto.randomUUID(),
+      name: name,
+      status: status,
+      selected: false,
+    }));
+} 
+
   return (
     <div className="form-container">
       <h2>Add item</h2>
-      <form action="">
-        <label htmlFor="title">Title:</label>
-        <input type="text" id="title" />
+      <form action={HandleSubmit}>
+        <label htmlFor="name">Title:</label>
+        <input type="text" id="name" name="name" required/>
         <label htmlFor="status">Status:</label>
         <select name="status" id="status" title="status">
           {Object.values(Status).map((value) => (
