448736c1868554f3265121c62ec42178012e9cc8
Rose Phillips <rophillips@ybs.co.uk>
2026-06-25 15:50:40 +0100

    added useEffect to get data from json server. Added .map to compare item.status to listTitle and render the item.

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,14 +1,35 @@
 import type { ListTypes } from "../types"
 import { Item } from "./Item"
+import { useDispatch, useSelector } from "react-redux";
+import { setItems } from "../redux/ItemSlice";
+import type { RootState } from "../redux/store";
+import { useEffect } from "react";
 
 
 //if the item status matches the list title it get pulled into the list
 
 export const List = ({ listTitle }: ListTypes) => {
+
+    const dispatch = useDispatch();
+  
+    const items = useSelector((state: RootState) => state.item.items);
+  
+      useEffect(() => {
+      fetch('http://localhost:3000/items')
+        .then((response) => response.json())
+        .then((data) => dispatch(setItems(data)))
+        .catch((error) => console.log(error));
+    }, [dispatch]);
   return (
     <section className="list-container">
       <h3 className="list-title">{listTitle}</h3>
-      <Item/>
+     {items.map((item) => { 
+    if (item.status === listTitle) {
+      return <Item key={item.id} item={item} />;
+    }
+    return null; 
+  })}
+      
     </section>
     
   )
