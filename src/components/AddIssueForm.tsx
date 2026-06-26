2dad4d4db15c194bf951854fe5cb37aa67d51c9b
armani-altaf <aaltaf@ybs.co.uk>
2026-06-26 10:12:05 +0100

    post-function

--- src/components/AddIssueForm.tsx
+++ src/components/AddIssueForm.tsx
@@ -1,37 +1,45 @@
 import { useDispatch } from "react-redux";
 import { addItem } from "../redux/ItemSlice";
 import { Status } from "../types";
-import type {StatusType} from "../types"
+import type { StatusType } from "../types"
 
 export const AddIssueForm = () => {
 
-const dispatch = useDispatch();
 
-const HandleSubmit = (data: FormData) => {
+  const dispatch = useDispatch();
+
+  const HandleSubmit = async (data: FormData) => {
     const name = data.get("name") as string;
     const status = data.get("status") as StatusType;
-    dispatch(addItem({
+    const newItem = {
       id: crypto.randomUUID(),
       name: name,
       status: status,
       selected: false,
-    }));
-} 
+    }
+    dispatch(addItem(newItem));
+    const res = fetch('http://localhost:3000/items', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify(newItem),
+    })
+}
 
-  return (
-    <div className="form-container">
-      <h2>Add item</h2>
-      <form action={HandleSubmit}>
-        <label htmlFor="name">Task name:</label>
-        <input type="text" id="name" name="name" required/>
-        <label htmlFor="status">Task status:</label>
-        <select name="status" id="status" title="status">
-          {Object.values(Status).map((value) => (
-            <option key={value}>{value}</option>
-          ))}
-        </select>
-        <input className="form-button" type="submit" value="Add" />
-      </form>
-    </div>
-  );
+
+return (
+  <div className="form-container">
+    <h2>Add item</h2>
+    <form action={HandleSubmit}>
+      <label htmlFor="name">Task name:</label>
+      <input type="text" id="name" name="name" required />
+      <label htmlFor="status">Task status:</label>
+      <select name="status" id="status" title="status">
+        {Object.values(Status).map((value) => (
+          <option key={value}>{value}</option>
+        ))}
+      </select>
+      <input className="form-button" type="submit" value="Add" />
+    </form>
+  </div>
+);
 };
